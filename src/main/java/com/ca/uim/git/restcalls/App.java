a96aa138ac4ab3853074b3b453165bf8c779c655
Vijaya Kumar <batvi03@ca.com>
2018-04-16 13:11:16 +0530

    Terminating the application

--- src/main/java/com/ca/uim/git/restcalls/App.java
+++ src/main/java/com/ca/uim/git/restcalls/App.java
@@ -6,20 +6,21 @@ import java.util.concurrent.ExecutionException;
 import org.json.simple.parser.ParseException;
 
 import com.ca.uim.git.datastore.BranchStore;
-import com.ca.uim.git.model.Branch;
 import com.ca.uim.git.model.Organization;
 
 /**
- * Hello world!
+ * This is utility that helps to identify the stale branches in repositories
+ * under organization. It Generates reports and store data in Excel Spreadsheet
+ * under User_Home directory
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException, ParseException, InterruptedException, ExecutionException, java.text.ParseException 
-    {
-        GitRest gitRest = new GitRest();
-        Organization org = gitRest.gitRestCall(args[0],args[1],args[2],Integer.valueOf(args[3]));
-        BranchStore branchStore= new BranchStore();
-        branchStore.storeData(org);
-    }
+public class App {
+	public static void main(String[] args)
+			throws IOException, ParseException, InterruptedException, ExecutionException, java.text.ParseException {
+		GitRest gitRest = new GitRest();
+		Organization org = gitRest.gitRestCall(args[0], args[1], args[2], Integer.valueOf(args[3]));
+		BranchStore branchStore = new BranchStore();
+		branchStore.storeData(org);
+		System.exit(0);
+	}
 }
